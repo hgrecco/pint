1bbd27d3ba7ad703e54cc540a40abdfbe9ea8528
Hernan Grecco <hernan.grecco@gmail.com>
2013-04-27 21:11:32 -0300

    Added mathjax to sphinx configuration to show math expressions in the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ import datetime
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'sphinx.ext.viewcode', 'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
