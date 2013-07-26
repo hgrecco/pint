ab0d05b6c3cd05045e5863b62467e973b4d83e10
Hernan Grecco <hernan.grecco@gmail.com>
2013-07-26 20:09:37 -0300

    Added test for Issue #37

--- pint/testsuite/test_issues.py
+++ pint/testsuite/test_issues.py
@@ -8,9 +8,12 @@ from pint import UnitRegistry
 from pint.unit import UnitsContainer
 from pint.util import ParserHelper
 
+from pint.testsuite import HAS_NUMPY, np, TestCase
+
 class TestIssues(unittest.TestCase):
 
-    def _test_issue25(self):
+    @unittest.expectedFailure
+    def test_issue25(self):
         x = ParserHelper.from_string('10 %')
         self.assertEqual(x, ParserHelper(10, {'%': 1}))
         x = ParserHelper.from_string('10 ‰')
@@ -31,3 +34,22 @@ class TestIssues(unittest.TestCase):
         self.assertEqual(t.magnitude, 4)
         self.assertEqual(t.units, UnitsContainer(millimolar=1))
         self.assertEqual(t.to('mole / liter'), 4e-3 * ureg['M'])
+
+
+@unittest.skipUnless(HAS_NUMPY, 'Numpy not present')
+class TestIssuesNP(TestCase):
+
+    FORCE_NDARRAY = False
+
+    @unittest.expectedFailure
+    def test_issue37(self):
+        x = np.ma.masked_array([1, 2, 3], mask=[True, True, False])
+        ureg = UnitRegistry()
+        q = ureg.meter * x
+        self.assertIsInstance(q, ureg.Quantity)
+        self.assertSequenceEqual(q.magnitude, x)
+        self.assertEquals(q.units, ureg.meter.units)
+        q = x * ureg.meter
+        self.assertIsInstance(q, ureg.Quantity)
+        self.assertSequenceEqual(q.magnitude, x)
+        self.assertEquals(q.units, ureg.meter.units)
