5929ce5813fc10c583e61b44734a39ee2b61c1e9
Hernan Grecco <hernan.grecco@gmail.com>
2013-12-17 18:59:45 -0300

    Back to development: 0.5

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ __doc__ = long_description
 
 setup(
     name='Pint',
-    version='0.4',
+    version='0.5.dev0',
     description='Physical quantities module',
     long_description=long_description,
     keywords='physical quantities unit conversion science',
