fb95fbbf82e5c63bd071b904798a75dcd63160b5
Hernan Grecco <hernan.grecco@gmail.com>
2014-08-10 12:18:55 -0300

    Removed fetching version from git repo
    If  git isn't installed, OS X prompts the user to install Xcode.
    We choose to remove this developer feature to make it better for the end user.
    
    See #47
    Close #178

--- pint/__init__.py
+++ pint/__init__.py
@@ -22,19 +22,12 @@ from .util import pi_theorem, logger
 from .context import Context
 
 
-__version__ = "unknown"
-try:                    # pragma: no cover
-    # try to grab the commit version of our package
-    __version__ = (subprocess.check_output(["git", "describe"],
-                                           stderr=subprocess.STDOUT,
-                                           cwd=os.path.dirname(os.path.abspath(__file__)))).strip()
-except:                 # pragma: no cover
-    # on any error just try to grab the version that is installed on the system
-    try:
-        __version__ = pkg_resources.get_distribution('pint').version
-    except:             # pragma: no cover
-        pass  # we seem to have a local copy without any repository control or installed without setuptools
-              # so the reported version will be __unknown__
+try:                # pragma: no cover
+    __version__ = pkg_resources.get_distribution('pint').version
+except:             # pragma: no cover
+    # we seem to have a local copy not installed without setuptools
+    # so the reported version will be unknown
+    __version__ = "unknown"
 
 
 #: A Registry with the default units and constants.
