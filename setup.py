e7e7de5ca2e1c19963be8a918369fb19186f9a73
Hernan Grecco <hernan.grecco@gmail.com>
2014-11-07 21:14:05 -0300

    Preparing release 0.6

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ __doc__ = long_description
 
 setup(
     name='Pint',
-    version='0.6a1',
+    version='0.6',
     description='Physical quantities module',
     long_description=long_description,
     keywords='physical quantities unit conversion science',
