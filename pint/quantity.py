26c85197442ba38d5485b6fc94c2d329a47030d8
Emilien Kofman <emilien.kofman@gmail.com>
2015-03-11 14:48:31 +0100

    Properly deepcopying

--- pint/quantity.py
+++ pint/quantity.py
@@ -119,7 +119,10 @@ class _Quantity(object):
         return ret
 
     def __deepcopy__(self, memo):
-        return self.__copy__()
+        ret = self.__class__(copy.deepcopy(self._magnitude), 
+                copy.deepcopy(self._units))
+        ret.__used = self.__used
+        return ret
 
     def __str__(self):
         return format(self)
