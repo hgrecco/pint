09b657a91ad4ccacc99276807cf0da9b5c0c31db
Hernan Grecco <hernan.grecco@gmail.com>
2016-02-20 19:55:43 -0300

    Updated setup.py to add Python 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -67,4 +67,6 @@ setup(
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ])
