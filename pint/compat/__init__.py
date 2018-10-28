081aba14dbef0518637b2cf80d3e81d2cd418855
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-28 17:15:43 +0100

    Pin Pandas version

--- pint/compat/__init__.py
+++ pint/compat/__init__.py
@@ -127,10 +127,7 @@ if not HAS_PROPER_BABEL:
 try:
     import pandas as pd
     HAS_PANDAS = True
-    HAS_PROPER_PANDAS = (
-        hasattr(pd.core, "arrays")
-        and hasattr(pd.core.arrays.base, "ExtensionOpsMixin")
-    )
+    HAS_PROPER_PANDAS = pd.__version__ == "0.24.0.dev0+625.gbdb7a16"
 except ImportError:
     HAS_PROPER_PANDAS = HAS_PANDAS = False
 
