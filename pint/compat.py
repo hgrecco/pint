6f1e41ed03a057a0236a36d399df7b5b57c16650
Keewis <keewis@posteo.de>
2019-12-24 00:06:09 +0100

    add a docstring for is_upcast_type

--- pint/compat.py
+++ pint/compat.py
@@ -147,6 +147,10 @@ if not HAS_BABEL:
 
 
 def is_upcast_type(other):
+    """ check if the type object is a upcast type
+
+    :param other: type
+    """
     # Check if class name is in preset list
     return other.__name__ in ("PintArray", "Series", "DataArray")
 
