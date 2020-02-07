95b56cabd961f775b6488174a7311456f614b9ce
5igno <signobox@gmail.com>
2020-02-07 12:19:25 +0100

    Implements LogarithmicConverter

--- pint/converters.py
+++ pint/converters.py
@@ -8,6 +8,8 @@
     :license: BSD, see LICENSE for more details.
 """
 
+from .compat import log, exp, HAS_NUMPY
+
 
 class Converter:
     """Base class for value converters."""
@@ -59,7 +61,7 @@ class ScaleConverter(Converter):
 
 class OffsetConverter(Converter):
     """An affine transformation
-    
+
     Parameters
     ----------
 
@@ -97,5 +99,72 @@ class OffsetConverter(Converter):
             value /= self.scale
         else:
             value = (value - self.offset) / self.scale
+
+        return value
+
+
+class LogarithmicConverter(Converter):
+    """ Converts between linear units and logarithmic units, such as dB, octave, neper or pH.
+
+    Q_log = logfactor * log( Q_lin / scale ) / log(log_base)
+
+    Parameters
+    ----------
+
+    scale : float
+        unit of reference at denominator for logarithmic unit conversion
+
+    logbase : float
+        base of logarithm used in the logarithmic unit conversion
+
+    logfactor : float
+        factor multupled to logarithm for unit conversion
+
+    inplace : bool
+        controls if computation is done in place
+
+    """
+    def __init__(self, scale, logbase, logfactor):
+        """
+
+        Parameters
+        ----------
+
+        scale : float
+            unit of reference at denominator inside logarithm for unit conversion
+
+        logbase: float
+            base of logarithm used in unit conversion
+
+        logfactor: float
+            factor multiplied to logarithm for unit conversion
+        """
+
+        if HAS_NUMPY is False:
+            print("'numpy' package is not installed. Will use math.log() "
+                  "for logarithmic units.")
+
+        self.scale = scale
+        self.logbase = logbase
+        self.logfactor = logfactor
+
+    def to_reference(self, value, inplace=False):
+        if inplace:
+            value /= self.scale
+            value = log(value)
+            value *= self.logfactor / log(self.logbase)
+        else:
+            value = self.logfactor * log(value / self.scale) / log(self.logbase)
+
+        return value
+
+    def from_reference(self, value, inplace=False):
+        if inplace:
+            value /= self.logfactor
+            value *= self.logbase
+            value = exp(value)
+            value *= self.scale
+        else:
+            value = self.scale * exp(log(self.logbase) * (value / self.logfactor))
 
         return value
