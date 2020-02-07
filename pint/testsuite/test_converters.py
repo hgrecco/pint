0a99f3e1ab4e0c14fe648ddc8f77c5a2109bf64c
5igno <signobox@gmail.com>
2020-02-07 12:24:24 +0100

    Tests logaritmic converter

--- pint/testsuite/test_converters.py
+++ pint/testsuite/test_converters.py
@@ -1,7 +1,12 @@
 import itertools
 
 from pint.compat import np
-from pint.converters import Converter, OffsetConverter, ScaleConverter
+from pint.converters import (
+    Converter,
+    LogarithmicConverter,
+    OffsetConverter,
+    ScaleConverter,
+)
 from pint.testsuite import BaseTestCase, helpers
 
 
@@ -21,6 +26,19 @@ class TestConverter(BaseTestCase):
         c = OffsetConverter(20.0, 2)
         self.assertEqual(c.from_reference(c.to_reference(100)), 100)
         self.assertEqual(c.to_reference(c.from_reference(100)), 100)
+
+    def test_log_converter(self):
+        c = LogarithmicConverter(scale=1, logbase=10, logfactor=1)
+        self.assertEqual(c.from_reference(0), 1)
+        self.assertEqual(c.from_reference(1), 10.000000000000002)
+        self.assertEqual(c.from_reference(2), 100.00000000000004)
+        self.assertEqual(c.to_reference(1), 0)
+        self.assertEqual(c.to_reference(10), 1)
+        self.assertEqual(c.to_reference(100), 2)
+        arb_value = 3.14
+        c_arb_value = 3.1399999999999997
+        self.assertEqual(c.from_reference(c.to_reference(arb_value)), c_arb_value)
+        self.assertEqual(c.to_reference(c.from_reference(arb_value)), arb_value)
 
     @helpers.requires_numpy()
     def test_converter_inplace(self):
