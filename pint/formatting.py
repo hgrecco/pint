c1e6f1f07952ed7efb033e4273a218d3d8f5ac28
Porcelain Mouse <pmouse-github@nobig5.com>
2020-04-04 14:26:19 -0700

    Resolve Disagreement Between black and flake8

--- pint/formatting.py
+++ pint/formatting.py
@@ -89,7 +89,7 @@ _FORMATS = {
         "power_fmt": "{}^[{}]",
         "parentheses_fmt": r"\left({}\right)",
     },
-    "Lx": {"siopts": "", "pm_fmt": " +- ", },  # Latex format.
+    "Lx": {"siopts": "", "pm_fmt": " +- "},  # Latex format with SIunitx.
     "H": {  # HTML format.
         "as_ratio": True,
         "single_denominator": True,
