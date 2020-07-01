f3ce6035c6b07148ddd015f00029d17a85979e67
andrewgsavage <andrewgsavage@gmail.com>
2020-07-01 13:20:39 +0100

    typo

--- pint/quantity.py
+++ pint/quantity.py
@@ -233,7 +233,7 @@ class Quantity(PrettyIPython, SharedRegistryObject):
         inst.__used = False
         inst.__handling = None
 
-        if hasattr(inst._magnitude, '__iter__') and not isinstance(inst._magnitide, str):
+        if hasattr(inst._magnitude, '__iter__') and not isinstance(inst._magnitude, str):
             inst.__iter__ = inst._iter
         return inst
 
