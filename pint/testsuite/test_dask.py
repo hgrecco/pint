583e597f95d60b9f217ae000967fd7c3e8875603
Russell Manser <russell.p.manser@ttu.edu>
2020-07-06 10:53:57 -0500

    Reduce redundancy in Dask array tests

--- pint/testsuite/test_dask.py
+++ pint/testsuite/test_dask.py
@@ -24,32 +24,36 @@ def numpy_array():
     return np.arange(0, 25, dtype=float).reshape((5, 5)) + 5
 
 
-def test_has_dask_graph(dask_array):
-    """Test that a pint.Quantity wrapped Dask array has a Dask graph."""
+@pytest.mark.parametrize("component", ["__dask_graph__", "__dask_keys__"])
+def test_has_dask_components(dask_array, component):
+    """Test that a pint.Quantity wrapped Dask array has a Dask graph and keys"""
     q = ureg.Quantity(dask_array, units_)
-    assert q.__dask_graph__() == dask_array.__dask_graph__()
+
+    q_method = getattr(q, component)
+    component_q = q_method()
+
+    truth_method = getattr(dask_array, component)
+    component_truth = truth_method()
+
+    assert component_q == component_truth
 
 
 def test_has_no_dask_graph(numpy_array):
-    """Test that a pint.Quantity wrapped NumPy array does not have a Dask graph,
+    """Test that a pint.Quantity wrapped NumPy array does not have a Dask graph
     and that attempting to access it returns None.
     """
     q = ureg.Quantity(numpy_array, units_)
     assert q.__dask_graph__() is None
 
 
-def test_has_dask_keys(dask_array):
-    """Test that a pint.Quantity wrapped Dask array has Dask keys."""
-    q = ureg.Quantity(dask_array, units_)
-    assert q.__dask_keys__() == dask_array.__dask_keys__()
-
-
 def test_dask_scheduler(dask_array):
     """Test that a pint.Quantity wrapped Dask array has the correct default scheduler."""
     q = ureg.Quantity(dask_array, units_)
+
     scheduler = q.__dask_scheduler__
-    scheduler_name = f'{scheduler.__module__}.{scheduler.__name__}'
-    true_name = 'dask.threaded.get'
+    scheduler_name = f"{scheduler.__module__}.{scheduler.__name__}"
+
+    true_name = "dask.threaded.get"
 
     assert scheduler == dask.array.Array.__dask_scheduler__
     assert scheduler_name == true_name
@@ -62,25 +66,16 @@ def test_dask_optimize(dask_array):
     assert q.__dask_optimize__ == dask.array.Array.__dask_optimize__
 
 
-def test_compute(dask_array, numpy_array):
-    """Test the compute() method on a pint.Quantity wrapped Dask array."""
-    q = ureg.Quantity(dask_array, units_)
-    comps = add_five(q)
-    res = comps.compute()
-
-    assert np.all(res.m == numpy_array)
-    assert res.units == units_
-    assert q.magnitude is dask_array
-
-
-def test_persist(dask_array, numpy_array):
-    """Test the persist() method on a pint.Quantity wrapped Dask array.
-
-    For single machines, persist() is expected to return the computed result(s).
+@pytest.mark.parametrize("method", ["compute", "persist"])
+def test_convenience_methods(dask_array, numpy_array, method):
+    """Test convenience methods compute() and persist() on a pint.Quantity
+    wrapped Dask array.
     """
     q = ureg.Quantity(dask_array, units_)
+
     comps = add_five(q)
-    res = comps.persist()
+    obj_method = getattr(comps, method)
+    res = obj_method()
 
     assert np.all(res.m == numpy_array)
     assert res.units == units_
@@ -91,40 +86,18 @@ def test_visualize():
     pass
 
 
-def test_compute_exception(numpy_array):
-    """Test exception handling for calling compute() on a pint.Quantity wrapped object
-    that is not a dask.array.core.Array object.
+@pytest.mark.parametrize("method", ["compute", "persist", "visualize"])
+def test_exception_method_not_implemented(numpy_array, method):
+    """Test exception handling for convenience methods on a pint.Quantity wrapped
+    object that is not a dask.array.Array object.
     """
     q = ureg.Quantity(numpy_array, units_)
-    comps = add_five(q)
-    with pytest.raises(AttributeError) as excinfo:
-        comps.compute()
 
-    exctruth = "Method compute only implemented for objects of <class 'dask.array.core.Array'>, not <class 'numpy.ndarray'>"
-    assert str(excinfo.value) == exctruth
-
-
-def test_persist_exception(numpy_array):
-    """Test exception handling for calling persist() on a pint.Quantity wrapped object
-    that is not a dask.array.core.Array object.
-    """
-    q = ureg.Quantity(numpy_array, units_)
-    comps = add_five(q)
-    with pytest.raises(AttributeError) as excinfo:
-        comps.persist()
-
-    exctruth = "Method persist only implemented for objects of <class 'dask.array.core.Array'>, not <class 'numpy.ndarray'>"
-    assert str(excinfo.value) == exctruth
-
-
-def test_visualize_exception(numpy_array):
-    """Test exception handling for calling visualize() on a pint.Quantity wrapped object
-    that is not a dask.array.core.Array object.
-    """
-    q = ureg.Quantity(numpy_array, units_)
-    comps = add_five(q)
-    with pytest.raises(AttributeError) as excinfo:
-        comps.visualize()
-
-    exctruth = "Method visualize only implemented for objects of <class 'dask.array.core.Array'>, not <class 'numpy.ndarray'>"
-    assert str(excinfo.value) == exctruth
+    exctruth = (
+        f"Method {method} only implemented for objects of"
+        " <class 'dask.array.core.Array'>, not"
+        " <class 'numpy.ndarray'>"
+    )
+    with pytest.raises(AttributeError, match=exctruth):
+        obj_method = getattr(q, method)
+        obj_method()
