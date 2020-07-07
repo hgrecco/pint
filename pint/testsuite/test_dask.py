75e1c03dc594b6592b0918cf86270e879275d95e
Russell Manser <russell.p.manser@ttu.edu>
2020-07-07 17:21:10 -0500

    Add distributed tests. Separate tests for compute() and persist()

--- pint/testsuite/test_dask.py
+++ pint/testsuite/test_dask.py
@@ -4,9 +4,16 @@ import pytest
 
 from pint import UnitRegistry
 
-# Conditionally import NumPy and Dask
+# Conditionally import NumPy, Dask, and Distributed
 np = pytest.importorskip("numpy", reason="NumPy is not available")
 dask = pytest.importorskip("dask", reason="Dask is not available")
+distributed = pytest.importorskip("distributed", reason="Distributed is not available")
+
+from dask.distributed import Client  # isort:skip
+from distributed.client import futures_of  # isort:skip
+from distributed.utils_test import cluster, gen_cluster, loop  # isort:skip
+
+loop = loop  # flake8
 
 ureg = UnitRegistry(force_ndarray_like=True)
 units_ = "kilogram"
@@ -26,26 +33,16 @@ def numpy_array():
     return np.arange(0, 25, dtype=float).reshape((5, 5)) + 5
 
 
-@pytest.mark.parametrize("component", ["__dask_graph__", "__dask_keys__"])
-def test_has_dask_components(dask_array, component):
-    """Test that a pint.Quantity wrapped Dask array has a Dask graph and keys"""
+def test_is_dask_collection(dask_array):
+    """Test that a pint.Quantity wrapped Dask array is a Dask collection."""
     q = ureg.Quantity(dask_array, units_)
-
-    q_method = getattr(q, component)
-    component_q = q_method()
-
-    truth_method = getattr(dask_array, component)
-    component_truth = truth_method()
-
-    assert component_q == component_truth
+    assert dask.is_dask_collection(q)
 
 
-def test_has_no_dask_graph(numpy_array):
-    """Test that a pint.Quantity wrapped NumPy array does not have a Dask graph
-    and that attempting to access it returns None.
-    """
+def test_is_not_dask_collection(numpy_array):
+    """Test that other pint.Quantity wrapped objects are not Dask collections."""
     q = ureg.Quantity(numpy_array, units_)
-    assert q.__dask_graph__() is None
+    assert not dask.is_dask_collection(q)
 
 
 def test_dask_scheduler(dask_array):
@@ -68,34 +65,30 @@ def test_dask_optimize(dask_array):
     assert q.__dask_optimize__ == dask.array.Array.__dask_optimize__
 
 
-@pytest.mark.parametrize("method", ["compute", "persist"])
-def test_convenience_methods(dask_array, numpy_array, method):
-    """Test convenience methods compute() and persist() on a pint.Quantity
-    wrapped Dask array.
-    """
+def test_compute(dask_array, numpy_array):
+    """Test the compute() method on a pint.Quantity wrapped Dask array."""
     q = ureg.Quantity(dask_array, units_)
 
     comps = add_five(q)
-    obj_method = getattr(comps, method)
-    res = obj_method()
+    res = comps.compute()
 
     assert np.all(res.m == numpy_array)
+    assert not dask.is_dask_collection(res)
     assert res.units == units_
     assert q.magnitude is dask_array
 
 
-def test_compute_persist_equivalent_single_machine(dask_array, numpy_array):
-    """Test that compute() and persist() return the same result for calls made
-    on a single machine.
-    """
+def test_persist(dask_array, numpy_array):
+    """Test the persist() method on a pint.Quantity wrapped Dask array."""
     q = ureg.Quantity(dask_array, units_)
 
     comps = add_five(q)
-    res_compute = comps.compute()
-    res_persist = comps.persist()
+    res = comps.persist()
 
-    assert np.all(res_compute == res_persist)
-    assert res_compute.units == res_persist.units == units_
+    assert np.all(res.m == numpy_array)
+    assert dask.is_dask_collection(res)
+    assert res.units == units_
+    assert q.magnitude is dask_array
 
 
 def test_visualize(dask_array):
@@ -109,6 +102,18 @@ def test_visualize(dask_array):
     # These commands only work on Unix and Windows
     assert os.path.exists("mydask.png")
     os.remove("mydask.png")
+
+
+def test_compute_persist_equivalent(dask_array, numpy_array):
+    """Test that compute() and persist() return the same result."""
+    q = ureg.Quantity(dask_array, units_)
+
+    comps = add_five(q)
+    res_compute = comps.compute()
+    res_persist = comps.persist()
+
+    assert np.all(res_compute == res_persist)
+    assert res_compute.units == res_persist.units == units_
 
 
 @pytest.mark.parametrize("method", ["compute", "persist", "visualize"])
@@ -126,3 +131,62 @@ def test_exception_method_not_implemented(numpy_array, method):
     with pytest.raises(AttributeError, match=exctruth):
         obj_method = getattr(q, method)
         obj_method()
+
+
+def test_distributed_compute(loop, dask_array, numpy_array):
+    """Test compute() for distributed machines."""
+    q = ureg.Quantity(dask_array, units_)
+
+    with cluster() as (s, [a, b]):
+        with Client(s["address"], loop=loop):
+            comps = add_five(q)
+            res = comps.compute()
+
+            assert np.all(res.m == numpy_array)
+            assert not dask.is_dask_collection(res)
+            assert res.units == units_
+
+    assert q.magnitude is dask_array
+
+
+def test_distributed_persist(loop, dask_array):
+    """Test persist() for distributed machines."""
+    q = ureg.Quantity(dask_array, units_)
+
+    with cluster() as (s, [a, b]):
+        with Client(s["address"], loop=loop):
+            comps = add_five(q)
+            persisted_q = comps.persist()
+
+            comps_truth = dask_array + 5
+            persisted_truth = comps_truth.persist()
+
+            assert np.all(persisted_q.m == persisted_truth)
+            assert dask.is_dask_collection(persisted_q)
+            assert persisted_q.units == units_
+
+    assert q.magnitude is dask_array
+
+
+@gen_cluster(client=True, timeout=None)
+async def test_async(c, s, a, b):
+    """Test asynchronous operations."""
+    da = dask.array.arange(0, 25, chunks=5, dtype=float).reshape((5, 5))
+    q = ureg.Quantity(da, units_)
+
+    x = q + ureg.Quantity(5, units_)
+    y = x.persist()
+    assert str(y)
+
+    assert dask.is_dask_collection(y)
+    assert len(x.__dask_graph__()) > len(y.__dask_graph__())
+
+    assert not futures_of(x)
+    assert futures_of(y)
+
+    future = c.compute(y)
+    w = await future
+    assert not dask.is_dask_collection(w)
+
+    truth = np.arange(0, 25, dtype=float).reshape((5, 5)) + 5
+    assert np.all(truth == w.m)
