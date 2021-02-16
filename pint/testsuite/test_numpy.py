Fix numpy.linalg.solve units output.
Update get_op_output_unit with new type invdiv.
It outputs the product of the following units over the first one in the args list.

Update tests with values & np.dot(A, x) == b.
Where x = np.linalg.solve(A, b)

Closes #1246

pint/testsuite/test_numpy.py
@@ -449,10 +449,13 @@ class TestNumpyMathematicalFunctions(TestNumpyMethods):
 
     @helpers.requires_array_function_protocol()
     def test_solve(self):
-        helpers.assert_quantity_almost_equal(
-            np.linalg.solve(self.q, [[3], [7]] * self.ureg.s),
-            self.Q_([[1], [1]], "m / s"),
-        )
+        A = self.q
+        b = [[3], [7]] * self.ureg.s
+        x = np.linalg.solve(A, b)
+
+        helpers.assert_quantity_almost_equal(x, self.Q_([[1], [1]], "s / m"))
+
+        helpers.assert_quantity_almost_equal(np.dot(A, x), b)
 
     # Arithmetic operations
     def test_addition_with_scalar(self):