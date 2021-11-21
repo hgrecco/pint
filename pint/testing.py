e687fa0b7a9d3d60595499d2af7e7067842b2482
Keewis <keewis@posteo.de>
2021-11-21 21:18:48 +0100

    expose `assert_quantity_almost_equal` as `pint.testing.assert_allclose`

--- pint/testing.py
+++ pint/testing.py
@@ -52,3 +52,38 @@ def assert_equal(first, second, msg=None):
         assert math.isnan(m1), msg
     else:
         assert m1 == m2, msg
+
+
+def assert_allclose(first, second, rtol=1e-07, atol=0, msg=None):
+    if msg is None:
+        try:
+            msg = "Comparing %r and %r. " % (first, second)
+        except TypeError:
+            try:
+                msg = "Comparing %s and %s. " % (first, second)
+            except Exception:
+                msg = "Comparing"
+
+    m1, m2 = _get_comparable_magnitudes(first, second, msg)
+    msg += " (Converted to %r and %r)" % (m1, m2)
+
+    if isinstance(m1, ndarray) or isinstance(m2, ndarray):
+        np.testing.assert_allclose(m1, m2, rtol=rtol, atol=atol, err_msg=msg)
+    elif not isinstance(m1, Number):
+        warnings.warn(RuntimeWarning)
+        return
+    elif not isinstance(m2, Number):
+        warnings.warn(RuntimeWarning)
+        return
+    elif math.isnan(m1):
+        assert math.isnan(m2), msg
+    elif math.isnan(m2):
+        assert math.isnan(m1), msg
+    elif math.isinf(m1):
+        assert math.isinf(m2), msg
+    elif math.isinf(m2):
+        assert math.isinf(m1), msg
+    else:
+        # Numpy version (don't like because is not symmetric)
+        # assert abs(m1 - m2) <= atol + rtol * abs(m2), msg
+        assert abs(m1 - m2) <= max(rtol * max(abs(m1), abs(m2)), atol), msg
