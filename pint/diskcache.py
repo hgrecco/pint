e655290fd92b0cee5ca61e4ecc561541623c4783
Hernan <hernan.grecco@gmail.com>
2022-02-12 17:05:03 -0300

    Implement a function to generate a cache file name for non pathlib.Path objects
    Given a dictionary, the cache filename will be generated by serializing
    with pickle the sorted dict keys. The values should contain mtime values
    used to decide if the cache file should be loaded or None.

--- pint/diskcache.py
+++ pint/diskcache.py
@@ -20,14 +20,19 @@ import pickle
 
 # TODO: provide a correct cache dir, maybe using appdirs
 CACHE_FOLDER = pathlib.Path().cwd()
+CACHE_FOLDER = pathlib.Path("/Users/grecco/Documents/code/pint")
 
 
-def cachepath_for(filepath: pathlib.Path) -> pathlib.Path:
+def cachepath_for(obj) -> pathlib.Path:
     """Generate a Path representing the location of a memoized file
-    for a given filepath.
+    for a given filepath or object.
     """
-    hd = hashlib.sha1(bytes(filepath.resolve())).hexdigest()
-    return CACHE_FOLDER.joinpath(hd).with_suffix(".pickle")
+    if isinstance(obj, pathlib.Path):
+        hd = hashlib.sha1(bytes(obj.resolve()))
+    else:
+        hd = hashlib.sha1(pickle.dumps(obj))
+
+    return CACHE_FOLDER.joinpath(hd.hexdigest()).with_suffix(".pickle")
 
 
 def load(original: pathlib.Path, ignore_date=False):
@@ -37,20 +42,63 @@ def load(original: pathlib.Path, ignore_date=False):
     If ignore_date is True, the file is loaded even if the cache
     is older thant the actual file.
     """
-    cache = cachepath_for(original)
-    if not cache.exists():
-        return
-    if ignore_date or cache.stat().st_mtime > original.stat().st_mtime:
-        with cache.open(mode="rb") as fi:
-            return pickle.load(fi)
-    return None
+
+    if ignore_date:
+        newest_date = 0
+    else:
+        newest_date = original.stat().st_mtime
+    return rawload(cachepath_for(original), newest_date)
 
 
 def save(obj, original: pathlib.Path) -> pathlib.Path:
     """Save the object (in pickle format) to the cache folder
     using a unique name generated using `cachepath_for`
     """
-    cache = cachepath_for(original)
-    with cache.open(mode="wb") as fo:
+    return rawsave(obj, cachepath_for(original))
+
+
+def load_referred(d: dict, ignore_date=False):
+    """Load and return the cached file if exists and it was created
+    after the original referring data structure.
+
+    The datastructure is summarized in a dict that containes the
+    modification time of different subelements as their values.
+
+    If ignore_date is True, the file is loaded even if the cache
+    is older thant the actual file.
+    """
+    keys = sorted(d.keys())
+    if ignore_date or not keys:
+        newest_date = 0
+    else:
+        newest_date = max(
+            (mtime for mtime in d.values() if mtime is not None), default=0
+        )
+    return rawload(cachepath_for(keys), newest_date)
+
+
+def save_referred(obj, d: dict):
+    """Save the object (in pickle format) to the cache folder
+    using a unique name generated using `cachepath_for`
+    """
+
+    keys = sorted(d.keys())
+    return rawsave(obj, cachepath_for(keys))
+
+
+def rawload(cachepath: pathlib.Path, newest_mtime):
+    if not cachepath.exists():
+        return
+    if cachepath.stat().st_mtime > newest_mtime:
+        with cachepath.open(mode="rb") as fi:
+            return pickle.load(fi)
+    return None
+
+
+def rawsave(obj, cachepath: pathlib.Path) -> pathlib.Path:
+    """Save the object (in pickle format) to the cache folder
+    using a unique name generated using `cachepath_for`
+    """
+    with cachepath.open(mode="wb") as fo:
         pickle.dump(obj, fo)
-    return cache
+    return cachepath
