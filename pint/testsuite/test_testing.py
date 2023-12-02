d8dd22d7f763fdebfaf69bd8c85fbb261594c34f
Hernan Grecco <hgrecco@gmail.com>
2023-12-02 10:22:50 -0300

    Migrate test_infer_base_unit to use sess_registry, not LazyRegistry

--- pint/testsuite/test_testing.py
+++ pint/testsuite/test_testing.py
@@ -1,10 +1,15 @@
 import pytest
 
-from pint import Quantity
+from typing import Any
 
 from .. import testing
 
 np = pytest.importorskip("numpy")
+
+
+class QuantityToBe(tuple[Any]):
+    def from_many(*args):
+        return QuantityToBe(args)
 
 
 @pytest.mark.parametrize(
@@ -14,7 +19,7 @@ np = pytest.importorskip("numpy")
             np.array([0, 1]), np.array([0, 1]), False, "", id="ndarray-None-None-equal"
         ),
         pytest.param(
-            Quantity(1, "m"),
+            QuantityToBe.from_many(1, "m"),
             1,
             True,
             "The first is not dimensionless",
@@ -22,73 +27,81 @@ np = pytest.importorskip("numpy")
         ),
         pytest.param(
             1,
-            Quantity(1, "m"),
+            QuantityToBe.from_many(1, "m"),
             True,
             "The second is not dimensionless",
             id="mixed2-int-not equal-equal",
         ),
         pytest.param(
-            Quantity(1, "m"), Quantity(1, "m"), False, "", id="Quantity-int-equal-equal"
-        ),
-        pytest.param(
-            Quantity(1, "m"),
-            Quantity(1, "s"),
-            True,
-            "Units are not equal",
-            id="Quantity-int-equal-not equal",
-        ),
-        pytest.param(
-            Quantity(1, "m"),
-            Quantity(2, "m"),
-            True,
-            "Magnitudes are not equal",
-            id="Quantity-int-not equal-equal",
-        ),
-        pytest.param(
-            Quantity(1, "m"),
-            Quantity(2, "s"),
-            True,
-            "Units are not equal",
-            id="Quantity-int-not equal-not equal",
-        ),
-        pytest.param(
-            Quantity(1, "m"),
-            Quantity(float("nan"), "m"),
-            True,
-            "Magnitudes are not equal",
-            id="Quantity-float-not equal-equal",
-        ),
-        pytest.param(
-            Quantity([1, 2], "m"),
-            Quantity([1, 2], "m"),
+            QuantityToBe.from_many(1, "m"),
+            QuantityToBe.from_many(1, "m"),
             False,
             "",
-            id="Quantity-ndarray-equal-equal",
+            id="QuantityToBe.from_many-int-equal-equal",
         ),
         pytest.param(
-            Quantity([1, 2], "m"),
-            Quantity([1, 2], "s"),
+            QuantityToBe.from_many(1, "m"),
+            QuantityToBe.from_many(1, "s"),
             True,
             "Units are not equal",
-            id="Quantity-ndarray-equal-not equal",
+            id="QuantityToBe.from_many-int-equal-not equal",
         ),
         pytest.param(
-            Quantity([1, 2], "m"),
-            Quantity([2, 2], "m"),
+            QuantityToBe.from_many(1, "m"),
+            QuantityToBe.from_many(2, "m"),
             True,
             "Magnitudes are not equal",
-            id="Quantity-ndarray-not equal-equal",
+            id="QuantityToBe.from_many-int-not equal-equal",
         ),
         pytest.param(
-            Quantity([1, 2], "m"),
-            Quantity([2, 2], "s"),
+            QuantityToBe.from_many(1, "m"),
+            QuantityToBe.from_many(2, "s"),
             True,
             "Units are not equal",
-            id="Quantity-ndarray-not equal-not equal",
+            id="QuantityToBe.from_many-int-not equal-not equal",
+        ),
+        pytest.param(
+            QuantityToBe.from_many(1, "m"),
+            QuantityToBe.from_many(float("nan"), "m"),
+            True,
+            "Magnitudes are not equal",
+            id="QuantityToBe.from_many-float-not equal-equal",
+        ),
+        pytest.param(
+            QuantityToBe.from_many([1, 2], "m"),
+            QuantityToBe.from_many([1, 2], "m"),
+            False,
+            "",
+            id="QuantityToBe.from_many-ndarray-equal-equal",
+        ),
+        pytest.param(
+            QuantityToBe.from_many([1, 2], "m"),
+            QuantityToBe.from_many([1, 2], "s"),
+            True,
+            "Units are not equal",
+            id="QuantityToBe.from_many-ndarray-equal-not equal",
+        ),
+        pytest.param(
+            QuantityToBe.from_many([1, 2], "m"),
+            QuantityToBe.from_many([2, 2], "m"),
+            True,
+            "Magnitudes are not equal",
+            id="QuantityToBe.from_many-ndarray-not equal-equal",
+        ),
+        pytest.param(
+            QuantityToBe.from_many([1, 2], "m"),
+            QuantityToBe.from_many([2, 2], "s"),
+            True,
+            "Units are not equal",
+            id="QuantityToBe.from_many-ndarray-not equal-not equal",
         ),
     ),
 )
-def test_assert_equal(first, second, error, message):
+def test_assert_equal(sess_registry, first, second, error, message):
+    if isinstance(first, QuantityToBe):
+        first = sess_registry.Quantity(*first)
+    if isinstance(second, QuantityToBe):
+        second = sess_registry.Quantity(*second)
     if error:
         with pytest.raises(AssertionError, match=message):
             testing.assert_equal(first, second)
