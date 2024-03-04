5e56155adb8b1e0c75b24db2018d446bb9716966
Enrico Massa <emassa@geckylabs.com>
2024-03-04 10:42:00 +0800

    Fix for linters

--- pint/testsuite/regressions/test_contexts.py
+++ pint/testsuite/regressions/test_contexts.py
@@ -2,10 +2,9 @@ from pint import UnitRegistry
 
 
 class TestRegressions:
-
     def test_lng_conversion(self):
         ureg = UnitRegistry()
-        ureg.load_definitions('pint/testsuite/regressions/conv.txt')
+        ureg.load_definitions("pint/testsuite/regressions/conv.txt")
         ureg.enable_contexts("lng")
-        u  = ureg.Unit("watt")
+        u = ureg.Unit("watt")
         assert u.compatible_units()
