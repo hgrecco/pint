23520e8bb11788b61d7361330fd63ad54aa4c579
Andrew <andrewgsavage@gmail.com>
2025-07-30 10:56:04 +0100

    fix

--- pint/testsuite/test_measurement.py
+++ pint/testsuite/test_measurement.py
@@ -304,4 +304,4 @@ class TestMeasurement(QuantityTestCase):
         m = func_registry.Measurement(5.0, 0.1, "meter")
         # quantity + measurement = measurement
         result = q + m
-        assert isinstance(result, func_registry.Measurement)
+        assert isinstance(result, type(m))
