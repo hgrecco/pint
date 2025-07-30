98e8132e7702f6e9cd3cd4634f70787791756712
Andrew <andrewgsavage@gmail.com>
2025-07-30 11:16:48 +0100

    Revert "ensure Measurement handles operations with Quantity (#2155)"
    This reverts commit c330a9bd1e863bd07f7cd4c3ed221e5739a61973.

--- pint/testsuite/test_measurement.py
+++ pint/testsuite/test_measurement.py
@@ -298,10 +298,3 @@ class TestMeasurement(QuantityTestCase):
         for p in pint_eval.tokenizer("8 + / - 4"):
             str(p)
         assert True
-
-    def test_measurement_result(self):
-        q = self.Q_(4.2, "meter")
-        m = self.Q_(5.0, "meter").plus_minus(0.1)
-        # quantity + measurement = measurement
-        result = q + m
-        assert isinstance(result, self.ureg.Measurement)
