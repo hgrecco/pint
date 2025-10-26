4565a2c3e83614f18cd1b7ef6efdf59628c1488d
Andrew <andrewgsavage@gmail.com>
2025-10-26 22:57:58 +0000

    add rmul test

--- pint/testsuite/typing_tests/quantity.py
+++ pint/testsuite/typing_tests/quantity.py
@@ -3,4 +3,4 @@ import pint
 
 ureg = pint.UnitRegistry() # type: pint.UnitRegistry
 position_vector: pint.Quantity[list[float]] = ureg.m * [1, 2, 3]
-# position_vector: pint.Quantity[list[float]] = [1, 2, 3] * ureg.m
+position_vector: pint.Quantity[list[float]] = [1, 2, 3] * ureg.m
